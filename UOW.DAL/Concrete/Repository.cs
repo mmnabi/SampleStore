fd06f650b6897136645d326350e9713ab567c2b4
mmnabi <mmnabi.cse@gmail.com>
2018-05-18 14:08:08 +0600

    Summary added to repository.

--- UOW.DAL/Concrete/Repository.cs
+++ UOW.DAL/Concrete/Repository.cs
@@ -14,41 +14,76 @@ namespace UOW.DAL.Concrete
             Context = context;
         }
 
+        /// <summary>
+        /// Adds entity into current context
+        /// </summary>
+        /// <param name="entity">Entity to add</param>
         public void Add(TEntity entity)
         {
             Context.Set<TEntity>().Add(entity);
         }
 
+        /// <summary>
+        /// Adds entity collection into current context
+        /// </summary>
+        /// <param name="entities">Collection of Entity to add</param>
         public void AddRange(System.Collections.Generic.IEnumerable<TEntity> entities)
         {
             Context.Set<TEntity>().AddRange(entities);
         }
 
+        /// <summary>
+        /// Fetches collection of entity using condition
+        /// </summary>
+        /// <param name="predicate">Linq expression</param>
+        /// <returns>Entity Collection</returns>
         public System.Collections.Generic.IEnumerable<TEntity> Find(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate)
         {
             return Context.Set<TEntity>().Where(predicate);
         }
 
+        /// <summary>
+        /// Gets an entity by Id
+        /// </summary>
+        /// <param name="id">Id of entity</param>
+        /// <returns>Entity</returns>
         public TEntity Get(int id)
         {
             return Context.Set<TEntity>().Find(id);
         }
 
+        /// <summary>
+        /// Gets all entitiy
+        /// </summary>
+        /// <returns>Entity Collection</returns>
         public System.Collections.Generic.IEnumerable<TEntity> GetAll()
         {
             return Context.Set<TEntity>().ToList();
         }
 
+        /// <summary>
+        /// Removes entity from current context
+        /// </summary>
+        /// <param name="entity">Db Entity</param>
         public void Remove(TEntity entity)
         {
             Context.Set<TEntity>().Remove(entity);
         }
 
+        /// <summary>
+        /// Removes entity collection from current context
+        /// </summary>
+        /// <param name="entities">Collection of Entity to remove</param>
         public void RemoveRange(System.Collections.Generic.IEnumerable<TEntity> entities)
         {
             Context.Set<TEntity>().RemoveRange(entities);
         }
 
+        /// <summary>
+        /// Gets Single or Default entity
+        /// </summary>
+        /// <param name="predicate">Linq expression</param>
+        /// <returns>Db Entity</returns>
         public TEntity SingleOrDefault(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate)
         {
             return Context.Set<TEntity>().SingleOrDefault(predicate);

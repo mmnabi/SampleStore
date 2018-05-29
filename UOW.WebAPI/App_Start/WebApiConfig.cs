0ed8472f86d9000a7b4e68174dc21b36f3960617
mmnabi <mmnabi.cse@gmail.com>
2018-05-29 14:27:32 +0600

    camelCase configured.

--- UOW.WebAPI/App_Start/WebApiConfig.cs
+++ UOW.WebAPI/App_Start/WebApiConfig.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
 using System.Web.Http.Cors;
+using Newtonsoft.Json.Serialization;
 
 namespace UOW.WebAPI
 {
@@ -18,8 +20,13 @@ namespace UOW.WebAPI
                 defaults: new { id = RouteParameter.Optional }
             );
 
+            // Enabling CORS
             EnableCorsAttribute cors=new EnableCorsAttribute("http://localhost:4200", "*", "*");
             config.EnableCors(cors);
+
+            // JSON camelCase
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
